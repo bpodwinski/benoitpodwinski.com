f8456e3b38842ea7df9712768dd9a739f2f60435
bpodwinski <me@benoitpodwinski.com>
2018-10-21 13:55:59 +0200

    init

---
+++ assets/js/src/Main.js
@@ -0,0 +1,54 @@
+var events = new Events();
+var simplexNoise = new SimplexNoise();
+
+var Main = function () {
+
+    function init() {
+
+        if (!Detector.webgl) {
+            Detector.addGetWebGLMessage();
+        }
+
+        //INIT DOCUMENT
+
+        window.addEventListener('resize', onResize, false);
+
+        //INIT HANDLERS
+        ControlsHandler.init();
+        VizHandler.init();
+        FXHandler.init();
+
+        onResize();
+
+        if (ControlsHandler.vizParams.showControls) {
+            $('#controls').show();
+        }
+
+        update();
+
+    }
+
+    function update() {
+        requestAnimationFrame(update);
+        events.emit("update");
+    }
+
+    function onResize() {
+        VizHandler.onResize();
+        FXHandler.onResize();
+    }
+
+    return {
+        init: init
+    };
+
+}();
+
+$(window).on('load', function() {
+  $('#status').fadeOut();
+  $('#preloader').delay(300).fadeOut('slow');
+})
+
+$(document).ready(function () {
+    setTimeout(Main.init, 100);
+});
---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')({
+    DEBUG: false,
+    pattern: '*'
+  });
+
+// BrowserSync
+gulp.task('browserSync', function() {
+  return plugins.browserSync.init({
+    host: '127.0.0.1',
+    proxy: 'benoitpodwinski.bp.local',
+    ghostMode: false,
+    open: false,
+    reloadOnRestart: true
+  });
+})
+
+// JS task
+gulp.task('js', function() {
+  return gulp.src([
+    'assets/js/src/lib/minivents.js',
+    'assets/js/src/lib/jquery-2.2.4.js',
+    'assets/js/src/lib/Detector.js',
+    'assets/js/src/lib/three.js',
+    'assets/js/src/lib/SimplexNoise.js',
+    'assets/js/src/lib/atutil.js',
+    'assets/js/src/lib/TweenMax.min.js',
+    'assets/js/src/lib/controls/OrbitControls.js',
+    'assets/js/src/lib/controls/DeviceOrientationControls.js',
+    'assets/js/src/lib/controls/VRControls.js',
+    'assets/js/src/lib/ViveController.js',
+    'assets/js/src/lib/isMobile.js',
+    'assets/js/src/viz/BG.js',
+    'assets/js/src/viz/Mecha.js',
+    'assets/js/src/viz/Shards.js',
+    'assets/js/src/viz/Assets.js',
+    'assets/js/src/Main.js',
+    'assets/js/src/ControlsHandler.js',
+    'assets/js/src/FXHandler.js',
+    'assets/js/src/VizHandler.js'
+  ])
+  .pipe(plugins.sourcemaps.init())
+  .pipe(plugins.plumber())
+  .pipe(plugins.concat('build.js'))
+  .pipe(plugins.uglify())
+  .pipe(plugins.sourcemaps.write('.'))
+  .pipe(gulp.dest('assets/js'));
+});
+
+// CSS task
+gulp.task('sass', function() {
+  return gulp.src('assets/sass/**/*.scss')
+  .pipe(plugins.sourcemaps.init())
+  .pipe(plugins.plumber())
+  .pipe(plugins.sass.sync({
+    includePaths: 'sass/styles.scss',
+    outputStyle: 'compressed'
+  })
+    .on('error', plugins.sass.logError)
+  )
+  .pipe(plugins.autoprefixer())
+  .pipe(plugins.rename({
+    basename: 'build',
+    extname: '.css'
+  }))
+  .pipe(plugins.sourcemaps.write('.'))
+  .pipe(gulp.dest('assets/css'))
+  .pipe(plugins.browserSync.stream());
+});
+
+// Default task
+gulp.task('default', ['browserSync', 'sass', 'js'], function() {
+  gulp.watch('assets/sass/**/*.scss', ['sass'])
+  gulp.watch('assets/js/src/**/*.js').on("change", plugins.browserSync.reload)
+  gulp.watch('*.html').on("change", plugins.browserSync.reload);
+});
