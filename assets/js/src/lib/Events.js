fd9388cd58942a4e27710a190a6d1285ccb95787
Benoit Podwinski <me@benoitpodwinski.com>
2025-01-25 20:01:52 +0100

    Refactor Event.js

--- assets/js/src/lib/Events.js
+++ assets/js/src/lib/Events.js
@@ -1,33 +1,52 @@
-function Events(target) {
-  var events = {},
-    empty = [];
-  target = target || this;
+class Events {
+  constructor(target) {
+    this.events = new Map(); // Utilise Map pour gérer les événements
+    this.target = target || this;
+  }
+
   /**
-   *  On: listen to events
+   * On: Listen to events
    */
-  target.on = function (type, func, ctx) {
-    (events[type] = events[type] || []).push([func, ctx]);
-    return target;
-  };
+  on(type, func, ctx = null) {
+    if (!this.events.has(type)) {
+      this.events.set(type, []);
+    }
+    this.events.get(type).push({ func, ctx });
+    return this.target;
+  }
+
   /**
-   *  Off: stop listening to event / specific callback
+   * Off: Stop listening to an event or a specific callback
    */
-  target.off = function (type, func) {
-    type || (events = {});
-    var list = events[type] || empty,
-      i = (list.length = func ? list.length : 0);
-    while (i--) func == list[i][0] && list.splice(i, 1);
-    return target;
-  };
+  off(type = null, func = null) {
+    if (!type) {
+      this.events.clear(); // Supprime tous les événements
+    } else if (this.events.has(type)) {
+      if (!func) {
+        this.events.delete(type); // Supprime tous les callbacks pour un type
+      } else {
+        // Supprime les callbacks spécifiques
+        const listeners = this.events.get(type);
+        this.events.set(
+          type,
+          listeners.filter((listener) => listener.func !== func)
+        );
+      }
+    }
+    return this.target;
+  }
+
   /**
-   * Emit: send event, callbacks will be triggered
+   * Emit: Trigger an event, callbacks will be executed
    */
-  target.emit = function (type) {
-    var e = events[type] || empty,
-      list = e.length > 0 ? e.slice(0, e.length) : e,
-      i = 0,
-      j;
-    while ((j = list[i++])) j[0].apply(j[1], empty.slice.call(arguments, 1));
-    return target;
-  };
+  emit(type, ...args) {
+    if (!this.events.has(type)) return this.target;
+
+    // Copie pour éviter les problèmes si des listeners sont modifiés pendant l'émission
+    const listeners = [...this.events.get(type)];
+    for (const { func, ctx } of listeners) {
+      func.apply(ctx, args);
+    }
+    return this.target;
+  }
 }
