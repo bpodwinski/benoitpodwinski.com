2791c3bd43a60af1cddca9de9f2bf0487d014be9
Benoit Podwinski <me@benoitpodwinski.com>
2025-01-26 17:26:57 +0100

    Add camera manager

---
+++ src/components/CameraManager.js
@@ -0,0 +1,38 @@
+import * as THREE from "three";
+
+export class CameraManager {
+  /**
+   * Initializes the camera.
+   * @param {number} width - The width of the rendering (aspect ratio).
+   * @param {number} height - The height of the rendering (aspect ratio).
+   * @param {number} fov - The field of view (default: 40).
+   * @param {number} near - The near clipping plane distance.
+   * @param {number} far - The far clipping plane distance.
+   */
+  constructor(width, height, fov = 40, near = 0.1, far = 1000) {
+    /**
+     * The perspective camera instance.
+     * @type {THREE.PerspectiveCamera}
+     */
+    this.camera = new THREE.PerspectiveCamera(fov, width / height, near, far);
+    this.camera.position.set(5, 5, 5);
+  }
+
+  /**
+   * Returns the initialized camera instance.
+   * @returns {THREE.PerspectiveCamera} The initialized camera.
+   */
+  getCamera() {
+    return this.camera;
+  }
+
+  /**
+   * Updates the camera's aspect ratio and projection matrix on resize.
+   * @param {number} width - The new width of the rendering.
+   * @param {number} height - The new height of the rendering.
+   */
+  updateAspectRatio(width, height) {
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+  }
+}
